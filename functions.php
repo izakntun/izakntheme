42132309d78074155bac1b8bc8cc6e8b3f3893ac
Isaac Cantun <izakntun@gmail.com>
2016-06-23 18:07:35 -0500

    se modifico la sidebar y ahora es dinamica

--- functions.php
+++ functions.php
@@ -5,4 +5,19 @@ function apk_load_styles()
     wp_enqueue_style('theme_style');
 }
 add_action('wp_enqueue_scripts', 'apk_load_styles');
+
+function apk_register_sidebars()
+{
+    register_sidebar(array
+    (
+        'name' => __('Sidebar principal' , 'apk'),
+        'id'   => 'main_sidebar',
+        'description' => __('Este es el sidebar principal' , 'apk'),
+        'before_widget' => '<div id = "%1$s" class = "widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class = "widget-title">',
+        'after_title'   => '</h3>'
+    ));
+}
+add_action('widgets_init', 'apk_register_sidebars');
 ?>
