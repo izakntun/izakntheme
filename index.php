3cee6a3614f0c85def1c0db3079e223eb4431bd6
Isaac Cantun <izakntun@gmail.com>
2016-06-24 11:56:38 -0500

    Se realizo el Loop de WordPress en el Theme

--- index.php
+++ index.php
@@ -2,71 +2,33 @@
 
 			<section id="main-content">
 
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<article class="post resume">
 					<header class="post-title">
-						<h2><a href="">El título de un post</a></h2>
-						<small class="meta">Marzo 22, 2014 &bull; <a href="">Categoría</a></small>
+						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+						<small class="meta"><?php the_time(get_option('date_format')); ?> &bull; <?php the_category(', '); ?></small>
 					</header>
 					<div class="post-content">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						<a href="" class="readmore">Seguir leyendo &rarr;</a>
+						<?php the_excerpt(); ?>
+						<a href="<?php the_permalink(); ?>" class="readmore"><?php _e('Seguir leyendo &rarr;', 'apk'); ?></a>
 					</div>
 				</article>	<!-- article -->
-
-
+			<?php endwhile; else: ?>
 				<article class="post resume">
 					<header class="post-title">
-						<h2><a href="">El título de un post</a></h2>
-						<small class="meta">Marzo 22, 2014 &bull; <a href="">Categoría</a></small>
+						<h2><?php _e('No hay contenidos disponibles', 'apk'); ?></a></h2>
 					</header>
 					<div class="post-content">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						<a href="" class="readmore">Seguir leyendo &rarr;</a>
+						<p><?php _e('No hay contenidos que correspondan con esta página, por favor realiza una búsqueda para encontrar lo que deseas ver:', 'apk'); ?></p>
+						<?php get_search_form(); ?>
 					</div>
 				</article>	<!-- article -->
-
-
-				<article class="post resume">
-					<header class="post-title">
-						<h2><a href="">El título de un post</a></h2>
-						<small class="meta">Marzo 22, 2014 &bull; <a href="">Categoría</a></small>
-					</header>
-					<div class="post-content">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						<a href="" class="readmore">Seguir leyendo &rarr;</a>
-					</div>
-				</article>	<!-- article -->
-
-
-				<article class="post resume">
-					<header class="post-title">
-						<h2><a href="">El título de un post</a></h2>
-						<small class="meta">Marzo 22, 2014 &bull; <a href="">Categoría</a></small>
-					</header>
-					<div class="post-content">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						<a href="" class="readmore">Seguir leyendo &rarr;</a>
-					</div>
-				</article>	<!-- article -->
-
-
-				<article class="post resume">
-					<header class="post-title">
-						<h2><a href="">El título de un post</a></h2>
-						<small class="meta">Marzo 22, 2014 &bull; <a href="">Categoría</a></small>
-					</header>
-					<div class="post-content">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						<a href="" class="readmore">Seguir leyendo &rarr;</a>
-					</div>
-				</article>	<!-- article -->
+			<?php endif; ?>
 
 				<div class="posts-nav cf">
 					<a href="" >&larr; Previos</a>
 					<a href="" >Recientes &rarr;</a>
 				</div>
-
-
 			</section><!-- /#main-content -->
 
 
--- sidebar.php
+++ sidebar.php
@@ -4,9 +4,12 @@
     if (is_active_sidebar('main_sidebar'))
     {
         dynamic_sidebar('main_sidebar');
-    }
-?>
-
+    } else {?>
+        <div class="widget">
+            <h3 class="widget-title"><?php _e('Buscar' , 'apk'); ?></h3>
+            <?php get_search_form(); ?>
+        </div>
+    <?php } ?>
 
 
 </aside><!-- /#sidebar -->
